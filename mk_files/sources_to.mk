fdd970be54e845841231d36e8b08794a74b418a8
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-11-12 23:02:57 +0100

    upgrade

--- mk_files/sources_to.mk
+++ mk_files/sources_to.mk
@@ -3,15 +3,15 @@
 #                                                         :::      ::::::::    #
 #    sources_to.mk                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: vduchi <vduchi@student.42barcelona.com>    +#+  +:+       +#+         #
+#    By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/28 17:59:25 by vduchi            #+#    #+#              #
-#    Updated: 2023/08/29 18:18:07 by vduchi           ###   ########.fr        #
+#    Updated: 2023/11/07 18:56:37 by nmota-bu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRCS_TO		=	srcs/to/ft_atoi.c
-SRCS_TO		=	srcs/to/ft_atol.c
+SRCS_TO		+=	srcs/to/ft_atol.c
 SRCS_TO		+=	srcs/to/ft_itoa.c
 SRCS_TO		+=	srcs/to/ft_tolower.c
 SRCS_TO		+=	srcs/to/ft_toupper.c
