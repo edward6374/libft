34c6ca9f7e738bdb1417942122b04251b85fd2c5
Valerio Duchi ☄️ <valerioduchi@gmail.com>
2023-12-22 12:08:26 +0100

    MK files updated

--- mk_files/sources_dptr.mk
+++ mk_files/sources_dptr.mk
@@ -6,14 +6,13 @@
 #    By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/28 17:59:25 by vduchi            #+#    #+#              #
-#    Updated: 2023/11/28 22:24:42 by nmota-bu         ###   ########.fr        #
+#    Updated: 2023/12/22 11:45:57 by vduchi           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRCS_DPTR	=	srcs/dptr/ft_len_dptr.c
 SRCS_DPTR	+=	srcs/dptr/ft_free_dptr.c
 SRCS_DPTR	+=	srcs/dptr/ft_print_dptr.c
-SRCS_DPTR	+=	srcs/dptr/ft_file_to_dptr.c
 SRCS_DPTR	+=	srcs/dptr/ft_file_to_dptr_m.c
 
 OBJS_DPTR	=	$(patsubst $(SRCS_DIR_DPTR)/%, $(OBJS_DIR_DPTR)/%, $(SRCS_DPTR:.c=.o))
